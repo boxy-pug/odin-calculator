9d0518fe10cccc389cc40b0946bd1cb43eaf1bbc
boxy-pug <151582186+boxy-pug@users.noreply.github.com>
2024-01-22 22:57:53 +0100

    Fixed most of the calculation functionality

--- main.js
+++ main.js
@@ -6,8 +6,10 @@
 
 // function add() {}
 
-const keys = document.querySelector("#calculator-container");
-const screen = document.querySelector("#screen");
+// const calculator = document.querySelector("#calculator-container");
+const calculator = document.querySelector(".calculator");
+const keys = calculator.querySelector("#calculator-container");
+const screen = calculator.querySelector("#screen");
 
 keys.addEventListener("click", (event) => {
   //   console.log(event.target);
@@ -16,11 +18,60 @@ keys.addEventListener("click", (event) => {
   const key = event.target;
   const keyValue = key.textContent;
   const screenValue = screen.textContent;
-  console.log(screenValue);
+  const type = key.dataset.type;
+  const { previousKeyType } = calculator.dataset;
 
-  if (screenValue === "0") {
-    screen.textContent = keyValue;
-  } else {
-    screen.textContent = screenValue + keyValue;
+  // If it's a number:
+  if (type === "number") {
+    if (screenValue === "0") {
+      screen.textContent = keyValue;
+    } else if (previousKeyType === "operator") {
+      screen.textContent = keyValue;
+    } else {
+      screen.textContent = screenValue + keyValue;
+    }
   }
+
+  // If it's an operator:
+  if (type === "operator") {
+    const operatorKeys = keys.querySelectorAll('[data-type="operator"]');
+    operatorKeys.forEach((el) => {
+      el.dataset.state = "";
+    });
+    key.dataset.state = "selected";
+    calculator.dataset.firstNumber = screenValue;
+    calculator.dataset.operator = key.dataset.key;
+  }
+
+  if (type === "equal") {
+    //perform calc
+    const firstNumber = calculator.dataset.firstNumber;
+    const operator = calculator.dataset.operator;
+    const secondNumber = screenValue;
+    console.log(firstNumber, operator, secondNumber);
+
+    let result = "";
+    if (operator === "plus") result = firstNumber + secondNumber;
+    if (operator === "minus") result = firstNumber - secondNumber;
+    if (operator === "divide") result = firstNumber / secondNumber;
+    if (operator === "times") result = firstNumber * secondNumber;
+    screen.textContent = calculate(firstNumber, operator, secondNumber);
+  }
+
+  if (type === "clear") {
+    screen.textContent = "";
+  }
+
+  calculator.dataset.previousKeyType = type;
 });
+
+function calculate(firstNumber, operator, secondNumber) {
+  firstNumber = parseInt(firstNumber);
+  secondNumber = parseInt(secondNumber);
+  let result = "0";
+  if (operator === "plus") result = firstNumber + secondNumber;
+  if (operator === "minus") result = firstNumber - secondNumber;
+  if (operator === "divide") result = firstNumber / secondNumber;
+  if (operator === "times") result = firstNumber * secondNumber;
+  return result;
+}
